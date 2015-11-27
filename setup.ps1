1fcc008c2897dd4b2b658d722dd34b7af2f1a0f0
Eddie Li <marubark@gmail.com>
2015-11-27 00:08:51 +0000

    gitconfig

--- setup.ps1
+++ setup.ps1
@@ -6,10 +6,15 @@ if (Test-Path $home\_gvimrc) {
 	Remove-Item $home\_gvimrc;
 }
 
+if (Test-Path $home\.gitconfig) {
+	Remove-Item $home\.gitconfig;
+}
+
 if (Test-Path $home\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1) {
 	Remove-Item $home\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1;
 }
 
 fsutil hardlink create $home\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 $home\dotfiles\Microsoft.PowerShell_profile.ps1;
+fsutil hardlink create $home\.gitconfig $home\dotfiles\.gitconfig;
 fsutil hardlink create $home\_vimrc $home\dotfiles\vim\.vimrc;
 fsutil hardlink create $home\_gvimrc $home\dotfiles\vim\.gvimrc;
