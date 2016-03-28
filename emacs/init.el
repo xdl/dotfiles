0a148f098166c97a2ef66ba1b424b2f6ea53206e
Eddie Li <marubark@gmail.com>
2016-03-28 18:00:37 +0100

    putting custom files elsewhere

--- emacs/init.el
+++ emacs/init.el
@@ -61,8 +61,7 @@
 (setq org-todo-keywords
       '((sequence "TODO" "IN-PROGRESS" "WAITING" "DONE")))
 
-;;https://www.emacswiki.org/emacs/ParEdit
-(require 'paredit)
+;https://www.emacswiki.org/emacs/ParEdit
 (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
 (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
 (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
@@ -71,15 +70,10 @@
 (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
 (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-agenda-files (quote ("~/orgs/calendar.org"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;;save custom variables elsewhere:
+;;if it doesn't exist:
+;http://stackoverflow.com/questions/14071991/how-to-create-an-empty-file-by-elisp
+(write-region "" nil "~/.emacs-custom.el")
+;;set and load it
+(setq custom-file "~/.emacs-custom.el")
+(load custom-file)
