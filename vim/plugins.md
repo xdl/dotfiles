1b41fa74e66e4a73feea4c552ea39f425cfd5e87
Eddie Li <marubark@gmail.com>
2016-09-03 23:01:04 +0100

    moving plugins to separate file

---
+++ vim/plugins.md
@@ -0,0 +1,39 @@
+# Plugins
+
+## Core
+
+```
+git clone https://github.com/scrooloose/nerdtree.git;
+git clone https://github.com/scrooloose/nerdcommenter.git;
+git clone https://github.com/ctrlpvim/ctrlp.vim.git;
+git clone https://github.com/tpope/vim-surround;
+git clone https://github.com/tpope/vim-repeat;
+git clone https://github.com/tpope/vim-fugitive;
+git clone https://github.com/jpalardy/vim-slime.git;
+git clone https://github.com/scrooloose/syntastic.git;
+git clone https://github.com/SirVer/ultisnips.git
+git clone https://github.com/xdl/vim-snippets.git;
+git clone https://github.com/bling/vim-airline;
+git clone https://github.com/morhetz/gruvbox.git;
+git clone https://github.com/sjl/badwolf;
+```
+
+(ultisnips requires Python)
+
+## Optional
+
+```
+git clone https://github.com/terryma/vim-expand-region;
+git clone https://github.com/xdl/vim-replr.git;
+git clone https://github.com/godlygeek/tabular.git;
+```
+
+## Syntax
+
+```
+git clone https://github.com/fatih/vim-go.git;
+git clone https://github.com/pangloss/vim-javascript;
+git clone https://github.com/leafgarland/typescript-vim.git;
+git clone https://github.com/mattn/emmet-vim.git;
+git clone https://github.com/PProvost/vim-ps1.git;
+```
--- vim/setup.sh
+++ vim/setup.sh
@@ -16,52 +16,5 @@ rm -rf ~/.vim/tmp;
 popd;
 
 # everything else
-echo "installing everything else..."
+echo "install everything else from plugins.md"
 pushd ~/.vim/bundle;
-
-# Barebones, low impact
-git clone https://github.com/scrooloose/nerdtree.git;
-git clone https://github.com/scrooloose/nerdcommenter.git;
-
-git clone https://github.com/ctrlpvim/ctrlp.vim.git;
-
-git clone https://github.com/tpope/vim-surround;
-git clone https://github.com/tpope/vim-repeat;
-git clone https://github.com/tpope/vim-fugitive;
-
-git clone https://github.com/jpalardy/vim-slime.git;
-
-git clone https://github.com/scrooloose/syntastic.git;
-
-git clone https://github.com/SirVer/ultisnips.git
-git clone https://github.com/xdl/vim-snippets.git;
-
-# Useful, but may cause performance hit
-
-git clone https://github.com/bling/vim-airline;
-
-# Rarely used
-
-git clone https://github.com/terryma/vim-expand-region;
-git clone https://github.com/xdl/vim-replr.git;
-git clone https://github.com/godlygeek/tabular.git;
-
-# Syntax
-
-#git clone https://github.com/fatih/vim-go.git;
-
-#git clone https://github.com/pangloss/vim-javascript;
-#git clone https://github.com/leafgarland/typescript-vim.git;
-
-#git clone https://github.com/mattn/emmet-vim.git;
-
-#git clone https://github.com/PProvost/vim-ps1.git;
-
-# Colorschemes
-
-git clone https://github.com/morhetz/gruvbox.git;
-git clone https://github.com/sjl/badwolf;
-
-popd;
-
-echo "done!"
