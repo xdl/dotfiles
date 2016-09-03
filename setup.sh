40aa9c872165429d00f3590eb046f5c0ef2801d2
Eddie Li <marubark@gmail.com>
2016-09-03 23:03:04 +0100

    unix setup

--- setup.sh
+++ setup.sh
@@ -4,12 +4,17 @@
 #rm -f ~/.bashrc;
 #ln -s ~/dotfiles/.bashrc ~/.bashrc;
 
+echo "creating symbolic link for tmux..."
 rm -f ~/.tmux.conf;
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf;
 
+echo "creating symbolic link for vim..."
 rm -f ~/.vimrc;
 ln -s ~/dotfiles/vim/.vimrc ~/.vimrc;
 
+echo "creating symbolic link for emacs..."
 mkdir -p ~/.emacs.d;
 rm -f ~/.emacs.d/init.el;
 ln -s ~/dotfiles/emacs/init.el ~/.emacs.d/init.el;
+
+echo "done!"
