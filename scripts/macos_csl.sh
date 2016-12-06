b0493d0418a75547263accbd8dd124b3aaffda90
Eddie Li <marubark@gmail.com>
2016-12-06 16:05:26 +0000

    adding get screenshot

--- scripts/macos_csl.sh
+++ scripts/macos_csl.sh
@@ -7,12 +7,13 @@ function usage ()
 
     Options:
     -h|help       Display this message
-    -s|screenshot <Screenshot Location> Sets screenshot path
+    -s|set-screenshot-location <Screenshot Location> Sets screenshot location
+    -g|get-screenshot-location  Gets screenshot path
     -v|version    Display script version"
 
 }    # ----------  end of function usage  ----------
 
-# directory needs to exist, otherwise screenshots will keep getting dumped to default location:
+# directory needs to exist, otherwise screenshots will keep getting dumped to default location: ~/Desktop
 function setScreenshotPath () {
     path=$1
     defaults write com.apple.screencapture location $path
@@ -20,15 +21,22 @@ function setScreenshotPath () {
     echo "com.apple.screencapture location set to $path"
 }
 
+# directory needs to exist, otherwise screenshots will keep getting dumped to default location:
+function getScreenshotPath () {
+    defaults read com.apple.screencapture location
+}
+
 #-----------------------------------------------------------------------
 #  Handle command line arguments
 #-----------------------------------------------------------------------
 
-while getopts ":hvs:" opt
+while getopts ":hvs:g" opt
 do
   case $opt in
 
-    s|screenshot   )  setScreenshotPath $OPTARG; exit 0 ;;
+    s|set-screenshot   )  setScreenshotPath $OPTARG; exit 0 ;;
+
+    g|get-screenshot   )  getScreenshotPath; exit 0 ;;
 
     h|help     )  usage; exit 0   ;;
 
