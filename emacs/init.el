4d3f66712750285682cb28daeef597026189c5d8
Eddie Li <eddie@xiaodi.li>
2018-02-25 20:36:09 +0000

    adding eslint configuration

--- emacs/init.el
+++ emacs/init.el
@@ -201,7 +201,30 @@
 ;;(ido-mode t)
 
 ;;Flycheck
-;;(require 'flycheck)
+(require 'flycheck)
+(global-flycheck-mode)
+
+;; disable jshint since we prefer eslint checking
+(setq-default flycheck-disabled-checkers
+  (append flycheck-disabled-checkers
+	  '(javascript-jshint)))
+
+
+;; use eslint with web-mode for jsx files
+(add-hook 'js2-mode-hook 'flycheck-mode)
+
+;; use local eslint from node_modules before global
+;; http://emacs.stackexchange.com/questions/21205/flycheck-with-file-relative-eslint-executable
+(defun my/use-eslint-from-node-modules ()
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (eslint (and root
+                      (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                        root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
+(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
 
 ;;Orgmode
 ;;-------
@@ -337,6 +360,7 @@
 ;; --------
 (require 'js2-mode)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(setq-default js2-strict-missing-semi-warning nil)
 
 ;;Misc
 ;;====
@@ -344,3 +368,4 @@
 (when (executable-find "hunspell")
   (setq-default ispell-program-name "hunspell")
   (setq ispell-really-hunspell t))
+
